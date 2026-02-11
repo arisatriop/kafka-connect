04523a0d12431adb2371efe8574c3fe66766a03f
Ari Satrio <arisatrioputra03@gmail.com>
2026-02-11 09:31:16 +0700

    first commit

---
+++ connectors/dbz-source-connector.sh
@@ -0,0 +1,54 @@
+
+
+# Config
+{
+  "name": "confluent-postgres-source",
+  "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+  "tasks.max": "1",
+  
+  "database.hostname": "postgres",
+  "database.port": "5432",
+  "database.user": "postgres",
+  "database.password": "postgres",
+  "database.dbname": "postgres",
+  
+  "topic.prefix": "confluent-cdc",
+  "schema.include.list": "public",
+  "table.include.list": "public.rates",
+  
+  "plugin.name": "pgoutput",
+  "slot.name": "confluent_rates_slot",
+  "publication.autocreate.mode": "filtered",
+  "snapshot.mode": "initial",
+  
+  "tombstones.on.delete": "true",
+  "time.precision.mode": "connect",
+  
+  "heartbeat.interval.ms": "30000",
+  "max.batch.size": "2048",
+  "max.queue.size": "8192",
+  "decimal.handling.mode": "double",
+  "include.schema.changes": "false"
+}
+
+
+
+
+# Minimum Config
+{
+  "name": "db_name.table_name.unique_suffix.source",
+  "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+  "tasks.max": "1",
+  "database.hostname": "postgres",
+  "database.port": "5432",
+  "database.user": "postgres",
+  "database.password": "postgres",
+  "database.dbname": "postgres",
+  "topic.prefix": "db_name",
+  "schema.include.list": "schema_name",
+  "table.include.list": "schema_name.table_name",
+  "plugin.name": "pgoutput",
+  "slot.name": "slot_table_name.unique_suffix",
+  "tombstones.on.delete": "true",
+  "time.precision.mode": "connect"
+}
---
+++ connectors/jdbc-sink-connector.json.sh
@@ -0,0 +1,72 @@
+
+
+# Config
+{
+    "name": "confluent-postgres-sink",
+    "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+    "tasks.max": "1",
+
+    "topics": "confluent-cdc.public.rates",
+
+    "connection.url": "jdbc:postgresql://postgres:5432/postgres",
+    "connection.user": "postgres",
+    "connection.password": "postgres",
+
+    "table.name.format": "rates_target",
+
+    "insert.mode": "upsert",
+    "pk.mode": "record_key",
+    "pk.fields": "id",
+
+    "delete.enabled": "true",
+    "auto.create": "false",
+    "auto.evolve": "true",
+
+    "transforms": "extractNewRecordState,selectFields",
+    "transforms.extractNewRecordState.type": "io.debezium.transforms.ExtractNewRecordState",
+    "transforms.extractNewRecordState.drop.tombstones": "false",
+    "transforms.extractNewRecordState.delete.handling.mode": "drop",
+    "transforms.selectFields.type": "org.apache.kafka.connect.transforms.ReplaceField$Value",
+    "transforms.selectFields.include": "id,name,created_at,created_by,updated_at,updated_by,deleted_at,deleted_by",
+
+    "quote.sql.identifiers": "always",
+    "batch.size": "1000",
+
+    "errors.tolerance": "all",
+    "errors.log.enable": "true",
+    "errors.log.include.messages": "true",
+    "errors.deadletterqueue.topic.name": "dlq.sink.errors",
+    "errors.deadletterqueue.topic.replication.factor": "1"
+}
+
+
+
+
+# Minimum Config
+{
+    "name": "db_name.table_name.sink",
+    "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+    "tasks.max": "1",
+    "topics": "topic_name",
+
+    "connection.url": "jdbc:postgresql://postgres:5432/db_name",
+    "connection.user": "secret",
+    "connection.password": "secret",
+    "table.name.format": "table_name",
+
+    "insert.mode": "upsert",
+    "pk.mode": "record_key",
+    "pk.fields": "id",
+
+    "delete.enabled": "true",
+    "auto.create": "false",
+    "auto.evolve": "false",
+    "batch.size": "1000",
+
+    "transforms": "extractNewRecordState,selectFields",
+    "transforms.extractNewRecordState.type": "io.debezium.transforms.ExtractNewRecordState",
+    "transforms.extractNewRecordState.drop.tombstones": "false",
+    "transforms.extractNewRecordState.delete.handling.mode": "drop",
+    "transforms.selectFields.type": "org.apache.kafka.connect.transforms.ReplaceField$Value",
+    "transforms.selectFields.include": "column1,column2,column3"
+}
